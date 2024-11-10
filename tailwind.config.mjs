4ad5f6df6f16c1ed4977da081a9e843ec99239ca
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2024-11-10 14:57:52 +1100

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        raidPrimary: "#368bcc",
+        raidSecondary: "#87b5cf",
+        raidGray: "#e7e6ea",
+        raidTextPrimary: "#4b86b1",
+      },
+    },
+  },
+  plugins: [],
+};
